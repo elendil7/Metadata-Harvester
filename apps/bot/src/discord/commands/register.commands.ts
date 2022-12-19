56c245db586808f244069659061881a73895641b
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-19 22:58:08 +0000

    Update fields & default function for every command

--- apps/bot/src/discord/commands/register.commands.ts
+++ apps/bot/src/discord/commands/register.commands.ts
@@ -1,11 +1,22 @@
 import { Client } from 'discord.js';
 
-export default class CommandHandler {
-	public client;
-	public name;
+export default class Command {
+	public name: string;
+	public aliases: string[];
+	public group: string;
+	public permissions: string[];
+	public description: string;
+	public emoji: string;
 
-	constructor(client: Client, name: string) {
-		this.client = client;
-		this.name = name;
+	constructor() {
+		this.name = 'exampleName';
+		this.aliases = ['exampleName1', 'exampleName2'];
+		this.group = 'exampleGroup';
+		this.permissions = ['examplePerm1', 'examplePerm2'];
+		this.description = 'This example command does things.';
+		this.emoji = '🔌';
+		this.run = this.run;
 	}
+
+	public async run(client: Client, ...args: any[]) {}
 }
