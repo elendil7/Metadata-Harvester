1cd6f75928e7ecb30e8599edd84feae31d12c817
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-19 07:13:37 +0000

    Updated to scan for commands in subdirectories

--- apps/bot/src/discord/utils/getFiles.ts
+++ apps/bot/src/discord/utils/getFiles.ts
@@ -1,24 +1,52 @@
 import { join } from 'path';
 import { readdirSync } from 'fs';
+import debug from 'debug';
+import { Collection } from 'discord.js';
 
-export const getEvents = () => {
+const LOG = debug('Metadata-Harvester:apps:bot:src:discord:utils:getFiles.ts');
+
+export const getEvents = async () => {
 	try {
+		// for every directory in events folder, get event name, return array of events
 		const path = join(__dirname, '../events');
 		return readdirSync(path, 'utf-8')
-			.filter((file) => file.endsWith('.ts'))
+			.filter(
+				(file) =>
+					file.endsWith('.ts') && !file.startsWith('register.events')
+			)
 			.map((file) => file.split('.ts')[0]);
 	} catch (e) {
-		console.error(e);
+		LOG(e);
 	}
 };
 
-export const getCommands = () => {
+export const getCommands = async () => {
 	try {
+		// for every directory AND subdirectory (1 level deep) in commands folder, get command names, return Collection of commandGroups (subdirectories) and their respective commands (files)
 		const path = join(__dirname, '../commands');
-		return readdirSync(path, 'utf-8')
-			.filter((file) => file.endsWith('.ts'))
-			.map((file) => file.split('.ts')[0]);
+
+		let commandGroups = new Collection<string, string[]>();
+
+		const subDirectories = readdirSync(path, 'utf-8').filter(
+			(file) => !file.endsWith('.ts')
+		);
+
+		// add each command (file) (string in array) to its resepctive commandGroup (directory) (string)
+		// example of commandGroups collection: Collection {commandGroup: [command1,command2,command3]}
+		subDirectories.forEach((dirName) => {
+			const commandsInDir = readdirSync(
+				`${path}/${dirName}`,
+				'utf-8'
+			).filter((file) => file.endsWith('.ts'));
+			commandGroups.set(dirName, commandsInDir);
+		});
+
+		return commandGroups;
+
+		/* readdirSync(`${path}/${dirName}`, 'utf-8').filter((file) =>
+					file.endsWith('.ts')
+				) */
 	} catch (e) {
-		console.error(e);
+		LOG(e);
 	}
 };
