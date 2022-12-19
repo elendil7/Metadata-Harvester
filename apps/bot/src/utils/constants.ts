20e0cca01f07d8a57d2cfa9dfecf1fba9d938ffd
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-19 07:13:08 +0000

    Altered some time-related constants

--- apps/bot/src/utils/constants.ts
+++ apps/bot/src/utils/constants.ts
@@ -28,33 +28,15 @@ export enum Symbols {
 	LOADING = `⌛`,
 }
 
-// time values
+// Time values
 export enum Convert_MS {
 	ms = 1,
-	milliseconds = 1,
-	s = 1000,
-	sec = 1000,
 	secs = 1000,
-	second = 1000,
-	seconds = 1000,
-	m = 60000,
-	min = 60000,
 	mins = 60000,
-	minute = 60000,
 	minutes = 60000,
-	h = 3600000,
-	hour = 3600000,
 	hours = 3600000,
-	d = 86400000,
-	day = 86400000,
 	days = 86400000,
-	w = 604800000,
-	week = 604800000,
 	weeks = 604800000,
-	mon = 2592000000,
-	month = 2592000000,
 	months = 2592000000,
-	y = 31536000000,
-	year = 31536000000,
 	years = 31536000000,
 }
