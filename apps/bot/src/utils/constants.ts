f261f0ca21dd28e2d3f0db2ceb1cf7622303af01
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-21 02:45:34 +0000

    Add discord colour codes & more emojis

--- apps/bot/src/utils/constants.ts
+++ apps/bot/src/utils/constants.ts
@@ -9,8 +9,9 @@ export const DISCORD_GUILD_ID = getEnv('DISCORD_GUILD_ID');
 
 // * For database
 
+// * Miscellaneous
 // Colours
-export enum Colors {
+export enum Colour_Hex {
 	RED = '#F4070D',
 	GREEN = '#95F3E3',
 	BlUE = '#08C4CD',
@@ -21,22 +22,54 @@ export enum Colors {
 	DARK_ORANGE = '#EE6B35',
 }
 
+export enum Colour_Codes {
+	DEFAULT = 0,
+	AQUA = 1752220,
+	DARKAQUA = 1146986,
+	GREEN = 5763719,
+	DARKGREEN = 2067276,
+	BLUE = 3447003,
+	DARKBLUE = 2123412,
+	PURPLE = 10181046,
+	DARKPURPLE = 7419530,
+	LUMINOUSVIVIDPINK = 15277667,
+	DARKVIVIDPINK = 11342935,
+	GOLD = 15844367,
+	DARKGOLD = 12745742,
+	ORANGE = 15105570,
+	DARKORANGE = 11027200,
+	RED = 15548997,
+	DARKRED = 10038562,
+	GREY = 9807270,
+	DARKGREY = 9936031,
+	DARKERGREY = 8359053,
+	LIGHTGREY = 12370112,
+	NAVY = 3426654,
+	DARKNAVY = 2899536,
+	YELLOW = 16776960,
+}
+
 // Symbols
 export enum Symbols {
 	SUCCESS = '✔️',
 	FAILURE = `❌`,
 	LOADING = `⌛`,
+	BUG = '🐛',
+	BOAR = '🐗',
+	DUCK = '🦆',
 }
 
 // Time values
 export enum Convert_MS {
-	ms = 1,
-	secs = 1000,
-	mins = 60000,
-	minutes = 60000,
-	hours = 3600000,
-	days = 86400000,
-	weeks = 604800000,
-	months = 2592000000,
-	years = 31536000000,
+	MS = 1,
+	SECS = 1000,
+	MINS = 60000,
+	HOURS = 3600000,
+	DAYS = 86400000,
+	WEEKS = 604800000,
+	MONTHS = 2592000000,
+	YEARS = 31536000000,
 }
+
+// Custom types
+export type numberstring = number | string;
