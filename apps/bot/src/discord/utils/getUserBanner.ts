46d2d34f06d0fe48bb0a330b0e6d26918a966ec6
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-22 15:12:14 +0000

    Create unconventional method to attain user banner

---
+++ apps/bot/src/discord/utils/getUserBanner.ts
@@ -0,0 +1,38 @@
+import { Message } from 'discord.js';
+import DiscordBot from '../structures/client';
+import { request } from 'undici';
+import { DISCORD_BOT_TOKEN, Images_Sizes } from '../../utils/constants';
+
+const getUserBanner = async (uid: string) => {
+	const { statusCode, headers, trailers, body } = await request(
+		`https://discord.com/api/v8/users/${uid}`,
+		{
+			headers: {
+				authorization: `Bot ${DISCORD_BOT_TOKEN}`,
+			},
+		}
+	);
+
+	// get data
+	const data: any = await body.json();
+	let banner = data['banner'];
+
+	if (statusCode === 200 && banner) {
+		// console.log(data);
+
+		// get info for http GET request for banner
+		let format = 'png';
+		if (banner.substring(0, 2) === 'a_') {
+			format = 'gif';
+		}
+		let size = Images_Sizes.XXLARGE;
+
+		// return user's banner
+		return `https://cdn.discordapp.com/banners/${uid}/${banner}.${format}?size=${size}`;
+	} else {
+		// return default banner
+		return 'https://whatifgaming.com/wp-content/uploads/2022/03/Living-the-Mountain-Life.jpg';
+	}
+};
+
+export default getUserBanner;
