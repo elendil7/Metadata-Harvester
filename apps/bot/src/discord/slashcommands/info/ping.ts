543f101d32d17a24c0816a0086e747e14a5ca46e
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-22 15:06:47 +0000

    Create 2 slash cmds - 1/2 complete

---
+++ apps/bot/src/discord/slashcommands/info/ping.ts
@@ -0,0 +1,21 @@
+import { CommandInteraction } from 'discord.js';
+import DiscordBot from '../../structures/client';
+import { SlashCommand } from '../../structures/slashcommand';
+import pingEmbededConstructor from '../../utils/embeds/info/ping';
+
+export default {
+	data: {
+		name: 'ping',
+		description: 'Gets bot ping in ms.',
+	},
+
+	run: async (
+		client: DiscordBot,
+		interaction: CommandInteraction,
+		args: any[]
+	) => {
+		interaction.reply({
+			embeds: [await pingEmbededConstructor(client, interaction)],
+		});
+	},
+} as SlashCommand;
---
+++ apps/bot/src/discord/slashcommands/info/whois.ts
@@ -0,0 +1,62 @@
+import { CommandInteraction } from 'discord.js';
+import DiscordBot from '../../structures/client';
+import { SlashCommand } from '../../structures/slashcommand';
+import whoisConstructor from '../../utils/embeds/info/whois';
+import getUserBanner from '../../utils/getUserBanner';
+import debugPath from '../../../utils/debugPath';
+const LOG = debugPath(__filename);
+
+export default {
+	data: {
+		// main command (arg 1)
+		name: 'whois',
+		description: 'Get basic info about selected user.',
+		// subcommands!
+		options: [
+			{
+				name: '@user',
+				description: 'Mention the user.',
+			},
+		],
+	},
+
+	run: async (
+		client: DiscordBot,
+		interaction: CommandInteraction,
+		args: any[]
+	) => {
+		try {
+			/* 			const target = interaction.mentions.users.first() || message.author;
+
+			// get user roles
+			const user = await message.guild!.members.fetch(target);
+			const roles = [
+				...user.roles.cache
+					.filter((role) => role.name !== '@everyone')
+					.values(),
+			]
+				.sort((a, b) => a.name.length - b.name.length)
+				.slice(0, 30)
+				.join('\n');
+
+			const guildUser = await message.guild!.members.fetch(target);
+
+			const bannerURL = await getUserBanner(target.id);
+
+			interaction.reply({
+				embeds: [
+					await whoisConstructor(
+						client,
+						interaction,
+						target,
+						guildUser,
+						roles,
+						bannerURL
+					),
+				],
+			}); */
+		} catch (e) {
+			LOG(e);
+		}
+	},
+} as SlashCommand;
