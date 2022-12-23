27be96cea68f55f467519892c82acf4f1f424aa1
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-23 21:22:52 +0000

    Add sleep method for mischief purposes

--- apps/bot/src/utils/sleep.ts
+++ apps/bot/src/utils/sleep.ts
@@ -1,6 +1,10 @@
-const sleep = (seconds: number) =>
+export const sleep = (seconds: number) =>
 	new Promise((res) => {
 		setTimeout(res, seconds * 1000);
 	});
 
-export default sleep;
+export const randomSleep = async (minSecs: number, maxSecs: number) => {
+	return await sleep(
+		Math.floor(Math.random() * (maxSecs - minSecs + 1) + minSecs)
+	);
+};
