3ec2f6e0da99ce07255db5529ff7597aea70ff36
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2022-12-24 23:32:01 +0000

    Fix slash command deletion methods

--- apps/bot/src/services/delete.slash.ts
+++ apps/bot/src/services/delete.slash.ts
@@ -1,30 +1,49 @@
-import { REST } from 'discord.js';
+import { REST, Routes } from 'discord.js';
 import DiscordBot from '../discord/structures/client';
 import {
 	DELETE_GLOBAL_SLASH_COMMANDS,
 	DELETE_GUILD_SLASH_COMMANDS,
+	DISCORD_BOT_ID,
 	DISCORD_BOT_TOKEN,
+	DISCORD_GUILD_IDS,
 } from '../utils/constants';
 import debugPath from '../utils/debugPath';
 const LOG = debugPath(__filename);
 
 const deleteSlashCommands = async (client: DiscordBot, commands: string[]) => {
-	// TODO const rest = new REST({ version: '10' }).setToken(DISCORD_BOT_TOKEN);
+	const rest = new REST({ version: '10' }).setToken(DISCORD_BOT_TOKEN);
 
-	// if config set to delete ALL guild slash commands, execute
-	if (DELETE_GUILD_SLASH_COMMANDS === 'true') {
-		client.application?.commands.set([]);
-		LOG(
-			`Regrettably deleted ${commands.length} <guild> slash (/) commands.`
-		);
-	}
+	try {
+		// if config set to delete ALL guild slash commands, execute
+		if (DELETE_GUILD_SLASH_COMMANDS === 'true') {
+			const guildIDs = DISCORD_GUILD_IDS.split(' ');
 
-	// if config set to delete ALL global slash commands, execute
-	if (DELETE_GLOBAL_SLASH_COMMANDS === 'true') {
-		client.application?.commands.set([]);
-		LOG(
-			`Regrettably deleted ${commands.length} <global> slash (/) commands.`
-		);
+			for (let i = 0; i < guildIDs.length; ++i) {
+				await rest.put(
+					Routes.applicationGuildCommands(
+						DISCORD_BOT_ID,
+						guildIDs[i]
+					),
+					{ body: [] }
+				);
+			}
+
+			LOG(
+				`Regrettably deleted ${commands.length} <guild> slash (/) commands.`
+			);
+		}
+
+		// if config set to delete ALL global slash commands, execute
+		if (DELETE_GLOBAL_SLASH_COMMANDS === 'true') {
+			await rest.put(Routes.applicationCommands(DISCORD_BOT_ID), {
+				body: [],
+			});
+			LOG(
+				`Regrettably deleted ${commands.length} <global> slash (/) commands.`
+			);
+		}
+	} catch (e) {
+		LOG(e);
 	}
 };
 
